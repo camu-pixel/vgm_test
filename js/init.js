4b740a0ec2e2728f9ca6dd69b7ab77e81e45435e
camu-pixel <66559584+camu-pixel@users.noreply.github.com>
2020-06-07 19:43:19 +0100

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,25 @@
+Amplitude.init({
+		songs: [
+			{
+				"name": "Down the Road",
+				"artist": "C2C",
+				"album": "Down The Road",
+				"url": "/song/Down the Road.mp3",
+				"cover_art_url": "/cover/down the road.png"
+			},
+			{
+				"name": "Harm in Change",
+				"artist": "Tori Y Moi",
+				"album": "Anything in Return",
+				"url": "/song/Harm In Change.mp3",
+				"cover_art_url": "/cover/harm in change.png"
+			},
+			{
+				"name": "They",
+				"artist": "Jem",
+				"album": "Finally Woken",
+				"url": "/song/They.mp3",
+				"cover_art_url": "/cover/they.png"
+			}
+		]
+	});
